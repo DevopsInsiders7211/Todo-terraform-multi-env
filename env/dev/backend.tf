93d6d0be38845d37a143a43547a8a374c1363082
Rishikesh Sharma <srishikesh39@gmai.com>
2025-10-03 13:30:22 +0530

    rG

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    access_key           = "KyZfhDVlbETYorG/kmtwhAgHkErpusX+NfD1F27or08HYXB/Huzd0+5e/PECyr/vI11vtlEiVNm4+AStUjjxXA=="  # Can also be set via `ARM_ACCESS_KEY` environment variable.
-    storage_account_name = "azurepipeline123"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "azure"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "prod.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    access_key           = "HOvWR6LIO+QZgMBo1f7Rh+1ZcHQezzy/XZ7BqbuRa3zP3JOBGL36o/eabdbkH01I4WYycKHxOS1X+AStulobDQ=="  # Can also be set via `ARM_ACCESS_KEY` environment variable.
+    storage_account_name = "appstg"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "appcontainer"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "Dev.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
