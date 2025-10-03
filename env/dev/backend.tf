13f63684465ce6eabf41c6e245705c6ade0526b0
Rishikesh Sharma <srishikesh39@gmai.com>
2025-10-03 13:31:46 +0530

    23

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    access_key           = "HOvWR6LIO+QZgMBo1f7Rh+1ZcHQezzy/XZ7BqbuRa3zP3JOBGL36o/eabdbkH01I4WYycKHxOS1X+AStulobDQ=="  # Can also be set via `ARM_ACCESS_KEY` environment variable.
+    # access_key           = "HOvWR6LIO+QZgMBo1f7Rh+1ZcHQezzy/XZ7BqbuRa3zP3JOBGL36o/eabdbkH01I4WYycKHxOS1X+AStulobDQ=="  # Can also be set via `ARM_ACCESS_KEY` environment variable.
     storage_account_name = "appstg"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "appcontainer"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "Dev.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
